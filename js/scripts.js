8bae42618e5a8209dd5c02c7b7e232cfb115a5df
Zach <zhedgepeth1124@gmail.com>
2016-09-01 10:56:54 -0400

    add semi-colons to all lines in need

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,31 @@
 var pingPong = function(numberInput) {
-  var numberOutput = []
+  var numberOutput = [];
   if (numberInput < 1) {
-    alert("Not a valid number! Try again!")
+    alert("Not a valid number! Try again!");
   }
 
   else {
     for (var numberIndex = 1; numberIndex <= numberInput; numberIndex++) {
       if (numberIndex % 15 === 0) {
         var pingPong = numberInput[numberIndex];
-        pingPong = "ping-pong"
-        numberOutput.push(pingPong)
+        pingPong = "ping-pong";
+        numberOutput.push(pingPong);
       }
       else if (numberIndex % 5 === 0) {
-        var pong = numberInput[numberIndex]
+        var pong = numberInput[numberIndex];
         pong = "pong";
-        numberOutput.push(pong)
+        numberOutput.push(pong);
       }
       else if (numberIndex % 3 === 0) {
-        var ping = numberInput[numberIndex]
+        var ping = numberInput[numberIndex];
         ping = "ping";
-        numberOutput.push(ping)
+        numberOutput.push(ping);
       } else {
-        numberOutput.push(numberIndex)
+        numberOutput.push(numberIndex);
       }
     }
 
-    return numberOutput.join("<li>")
+    return numberOutput.join("<li>");
   }
 }
 
@@ -37,10 +37,10 @@ $(document).ready(function() {
 
     var numberInput = parseInt($('#numberInput').val());
     $('#output ul').text("");
-    var result = pingPong(numberInput)
+    var result = pingPong(numberInput);
 
     var output = $('#output ul');
-    output.html("<p> " + result + "</p>")
+    output.html("<p> " + result + "</p>");
     var pingPongOutput = output.text().split(" ");
 
 

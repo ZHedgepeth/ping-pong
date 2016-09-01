11949bce191e0e38c5acb88e144500010de8f757
Zach <zhedgepeth1124@gmail.com>
2016-09-01 10:55:10 -0400

    rearrange scripts.js to include document.ready outside of the business logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,36 @@
+var pingPong = function(numberInput) {
+  var numberOutput = []
+  if (numberInput < 1) {
+    alert("Not a valid number! Try again!")
+  }
+
+  else {
+    for (var numberIndex = 1; numberIndex <= numberInput; numberIndex++) {
+      if (numberIndex % 15 === 0) {
+        var pingPong = numberInput[numberIndex];
+        pingPong = "ping-pong"
+        numberOutput.push(pingPong)
+      }
+      else if (numberIndex % 5 === 0) {
+        var pong = numberInput[numberIndex]
+        pong = "pong";
+        numberOutput.push(pong)
+      }
+      else if (numberIndex % 3 === 0) {
+        var ping = numberInput[numberIndex]
+        ping = "ping";
+        numberOutput.push(ping)
+      } else {
+        numberOutput.push(numberIndex)
+      }
+    }
+
+    return numberOutput.join("<li>")
+  }
+}
+
 $(document).ready(function() {
 
-var pingPong = function(numberInput) {
-    var numberOutput = []
-    if (numberInput < 1) {
-      alert("Not a valid number! Try again!")
-    }
-
-    else {
-      for (var numberIndex = 1; numberIndex <= numberInput; numberIndex++) {
-        if (numberIndex % 15 === 0) {
-          var pingPong = numberInput[numberIndex];
-          pingPong = "ping-pong"
-          numberOutput.push(pingPong)
-        }
-        else if (numberIndex % 5 === 0) {
-          var pong = numberInput[numberIndex]
-          pong = "pong";
-          numberOutput.push(pong)
-        }
-        else if (numberIndex % 3 === 0) {
-          var ping = numberInput[numberIndex]
-          ping = "ping";
-          numberOutput.push(ping)
-        } else {
-          numberOutput.push(numberIndex)
-        }
-      }
-
-      return numberOutput.join("<li>")
-    }
-  }
 
   $('#userNumber').submit(function(event) {
     event.preventDefault();
